ffb2dca7fbd5853b078b5bad30a853d3706068d9
Arlemon987 <pokoman533@gmail.com>
2026-05-02 13:22:52 +0600

    Update remove.js

--- api/remove.js
+++ api/remove.js
@@ -1,1 +1,18 @@
+export default async function handler(req, res) {
+    const apiKey = process.env.REMOVE_BG_API_KEY;
 
+    const formData = req.body;
+
+    const response = await fetch("https://api.remove.bg/v1.0/removebg", {
+        method: "POST",
+        headers: {
+            "X-Api-Key": apiKey
+        },
+        body: formData
+    });
+
+    const buffer = await response.arrayBuffer();
+
+    res.setHeader("Content-Type", "image/png");
+    res.status(200).send(Buffer.from(buffer));
+}
