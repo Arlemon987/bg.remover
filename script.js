349d939a66071e7382e9617b3a418ce7e8f3d770
Arlemon987 <pokoman533@gmail.com>
2026-05-02 13:21:28 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+const fileInput = document.getElementById("fileInput");
+const dropArea = document.getElementById("dropArea");
+const beforeImg = document.getElementById("before");
+const afterImg = document.getElementById("after");
+const loader = document.getElementById("loader");
+const downloadBtn = document.getElementById("downloadBtn");
+
+let file;
+
+// drag & drop
+dropArea.onclick = () => fileInput.click();
+
+fileInput.onchange = () => {
+    file = fileInput.files[0];
+    beforeImg.src = URL.createObjectURL(file);
+};
+
+// remove bg
+document.getElementById("removeBtn").onclick = async () => {
+    if (!file) return alert("Upload image first");
+
+    loader.style.display = "block";
+
+    const formData = new FormData();
+    formData.append("image", file);
+
+    const res = await fetch("/api/remove", {
+        method: "POST",
+        body: formData
+    });
+
+    const blob = await res.blob();
+    const url = URL.createObjectURL(blob);
+
+    afterImg.src = url;
+    loader.style.display = "none";
+
+    downloadBtn.style.display = "block";
+
+    downloadBtn.onclick = () => {
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "no-bg.png";
+        a.click();
+    };
+};
